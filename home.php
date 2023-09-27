35ef743c42564b1658875efe222af7aa52a808b1
raimeiga <yusuke612@hotmail.com>
2023-09-27 12:44:00 +0900

    6.3 ループでイベントの一覧を表示

--- home.php
+++ home.php
@@ -132,51 +132,54 @@
           <div class="col-lg-6 col-md-6 col-sx-12 news_col">
             <div class="home_title">Event</div>
             <div class="home_title_sub">イベント</div>
-            <div class="news_post_small">
-              <div class="row news_post_row">
-                <div class="col-lg-3 col-md-4 col-sx-12">
-                  <div class="calendar_border">
-                    <div class="calendar_border_1">
-                      <div class="calendar_month">OCT</div>
-                      <div class="calendar_day">
-                        <span>8</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-9 col-md-8 col-sx-12">
-                  <div class="news_post_small_title">
-                    <a href="events_detail.html">AWSハンズオンセミナー</a>
-                  </div>
-                  <div class="news_post_content">
-                    10月8日　新宿〇〇ビル8Fにて、AWSハンズオンセミナーを開催します。AWSを実際に構築してみます。参加希望者は...
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="news_post_small">
-              <div class="row news_post_row">
-                <div class="col-lg-3 col-md-4 col-sx-12">
-                  <div class="calendar_border">
-                    <div class="calendar_border_1">
-                      <div class="calendar_month">OCT</div>
-                      <div class="calendar_day">
-                        <span>15</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-lg-9 col-md-8 col-sx-12">
-                  <div class="news_post_small_title">
-                    <a href="events_detail.html">Raspberry Piハンズオンセミナー</a>
-                  </div>
-                  <div class="news_post_content">
-                    11月15日　新宿〇〇ビル8Fにて、Raspberry
-                    Piを使った、センサーを利用した自動走行ハンズオンセミナーを開催し...
-                  </div>
-                </div>
-              </div>
-            </div>
+           
+            <?php
+               //取得したい投稿記事などの条件を引数として渡す
+               $args = array(
+                   // 投稿タイプ
+                   'post_type'      => 'post',
+                   // カテゴリー名
+                   'category_name' => 'event',
+                   // 1ページに表示する投稿数
+                   'posts_per_page' => 2,
+               );
+               // データの取得
+               $posts = get_posts($args);
+             ?>
+ 
+             <!-- ループ処理 -->
+             <?php foreach($posts as $post): ?>
+               <?php setup_postdata($post); ?>
+               <div class="news_post_small">
+                 <div class="row news_post_row">
+                   <div class="col-lg-3 col-md-4 col-sx-12">
+                     <div class="calendar_border">
+                       <div class="calendar_border_1">
+                         <div class="calendar_month">OCT</div>
+                         <div class="calendar_day">
+                           <span>8</span>
+                         </div>
+                       </div>
+                     </div>
+                   </div>
+                   <div class="col-lg-9 col-md-8 col-sx-12">
+                     <div class="news_post_small_title">
+                       <a href="<?php the_permalink(); ?>">
+                         <?php the_title() ; ?>
+                       </a>
+                     </div>
+                     <div class="news_post_content">
+                       <?php 
+                         $content = wp_trim_words( get_the_content() ,  50, '...');
+                         echo $content;
+                       ?>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             <?php endforeach; ?>
+             <!-- 使用した投稿データをリセット -->
+             <?php wp_reset_postdata(); ?>
           </div>
           <!-- イベントここまで -->
         </div>
