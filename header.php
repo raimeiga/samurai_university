3476980bcac37a59dbe155d0dd10be5f96d2ca9b
raimeiga <yusuke612@hotmail.com>
2023-09-28 11:21:18 +0900

    ヘッダーに「卒業生の声」メニューを追加

--- header.php
+++ header.php
@@ -54,6 +54,12 @@
                   </a>
                 </li>
                 <li class="nav-item">
+                <a class="nav-link" href="<?php echo home_url(); ?>/category/graduates">
+                    GRADUATES
+                    <p>卒業生の声</p>
+                  </a>
+                </li>
+                <li class="nav-item">
                 <a class="nav-link" href="<?php echo home_url(); ?>/course">
                     COURSES
                     <p>コース</p>
